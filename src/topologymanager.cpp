2db01c5a690c6590033dbdd5532df178267e836d
lachlangrose <lachlan.grose@monash.edu>
2025-07-11 10:46:27 +0800

    adding framework to manager truncating surfaces

--- src/topologymanager.cpp
+++ src/topologymanager.cpp
@@ -1,4 +1,14 @@
-// #include "topologymanager.h"
+#include "topologymanager.h"
 
-// TopologyManager::TopologyManager(Grid grid) : _grid(grid) {}
+TopologyManager::TopologyManager(Grid &grid) : _grid(grid) {}
 
+Node TopologyManager::add_surface(std::string scalar_field_name, double isovalue) {
+    Node surface;
+    _surfaces.push_back(surface);
+    return surface
+}
+
+void TopologyManager::add_edge(Node surface, Node clipper) {
+    return
+
+}
--- src/topologymanager.h
+++ src/topologymanager.h
@@ -1,15 +1,22 @@
-// #ifndef TOPOLOGYMANAGER_H
-// #define TOPOLOGYMANAGER_H
-// #include "grid.h"
-// #include "marchingcubes.h"
-
-// class TopologyManager
-// {
-// public:
-//     // Constructor
-//     TopologyManager(Grid &grid);
-//     std::pair<std::string, TriMesh> getSurfaces() const;
-// private:
-//     Grid &_grid;
-// }
-// #endif // GRID_H
+#ifndef TOPOLOGYMANAGER_H
+#define TOPOLOGYMANAGER_H
+#include "grid.h"
+#include "marchingcubes.h"
+struct Node{
+    std::string scalar_field_name;
+    double isovalue;
+};
+class TopologyManager
+{
+public:
+    // Constructor
+    TopologyManager(Grid &grid);
+    Node add_surface(std::string scalar_field_name, double isovalue);
+    void add_edge(Node surface, Node clipper);
+    std::pair<std::string, TriMesh> getSurfaces() const;
+private:
+    Grid &_grid;
+    std::vector<Node> _surfaces;
+    std::pair<Node, Node> _topology;
+};
+#endif // GRID_H
